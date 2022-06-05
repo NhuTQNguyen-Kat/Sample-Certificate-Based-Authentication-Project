ac9d22dd2818933f7308e18605108327792b298a
nhu.tqnguyen <nhu.tqnguyen@C02G32CFML7H.local>
2022-06-05 21:59:31 +0700

    Create TS TSC

---
+++ Test Suites/Test Cert.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Cert</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>048c4873-e1d0-4f96-83f1-2afd1196b3ec</testSuiteGuid>
+</TestSuiteEntity>
