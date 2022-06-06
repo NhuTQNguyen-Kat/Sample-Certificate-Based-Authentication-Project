b84120ab40dd1c4b601dcf7d8c1f0f9a69b09003
nhu.tqnguyen <nhu.tqnguyen@C02G32CFML7H.local>
2022-06-06 10:15:00 +0700

    Create TestSuite

--- Test Suites/Test Cert.ts
+++ Test Suites/Test Cert.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>048c4873-e1d0-4f96-83f1-2afd1196b3ec</testSuiteGuid>
+   <testCaseLink>
+      <guid>0bdec941-8115-4e1d-a54b-c1783818b9f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Client Site</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
